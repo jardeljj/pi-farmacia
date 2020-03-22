9ee435e4e31b67fbfc7cfe09017581e61fe67d11
Felipe Canejo <felipecanejo@gmail.com>
2020-03-22 20:19:16 -0300

    Criação de classe carrinho e estoque

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,3 +1,6 @@
+
+import java.util.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,32 @@
  * @author fcane
  */
 public class Estoque {
+    public int idProduto;
+    public long qtdEstoque;
+    public Date dataAtualizacao;
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public long getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(long qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
     
 }
